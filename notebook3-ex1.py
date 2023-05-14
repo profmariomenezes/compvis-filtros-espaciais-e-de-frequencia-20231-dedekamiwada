a33e38a8065c82ce4fe41fcf31fef9ab0f48198f
dedekamiwada <90073001+dedekamiwada@users.noreply.github.com>
2023-05-13 22:45:13 -0300

    Update notebook3-ex1.py

--- notebook3-ex1.py
+++ notebook3-ex1.py
@@ -1,36 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fft import fft2, fftshift
 
-# Criar imagem 30x30 com um retângulo de 5:24 em x e de 13:17 em y
+# Criação da imagem
 img = np.zeros((30,30))
 img[13:18, 5:25] = 1
 
-# Calcular Transformada Rápida de Fourier (FFT2)
-fft_img = np.fft.fft2(img)
+# Cálculo da Transformada de Fourier e Magnitude do Espectro
+fft_img = fft2(img)
+magnitude_spectrum = np.abs(fft_img)
 
-# Calcular a magnitude do espectro da imagem original
-mag_spectrum = np.abs(fft_img)
-
-# Padding da imagem com zeros para aumentar a resolução
+# Padding da imagem
 padded_img = np.pad(img, ((113, 113), (113, 113)), 'constant')
-padded_fft_img = np.fft.fft2(padded_img)
+padded_fft_img = fft2(padded_img)
+padded_magnitude_spectrum = np.abs(padded_fft_img)
 
-# Calcular a magnitude do espectro da imagem com padding
-padded_mag_spectrum = np.abs(padded_fft_img)
+# Diferenças dos espectros
+print("Tamanho da imagem original:", img.shape)
+print("Tamanho da imagem com padding:", padded_img.shape)
+print("Tamanho do espectro da imagem original:", magnitude_spectrum.shape)
+print("Tamanho do espectro da imagem com padding:", padded_magnitude_spectrum.shape)
 
-# Shift na Transformada, colocando a origem no centro da imagem
-shifted_fft_img = np.fft.fftshift(padded_fft_img)
+# Shift da Transformada
+shifted_fft_img = fftshift(padded_fft_img)
+shifted_magnitude_spectrum = np.abs(shifted_fft_img)
 
-# Transformação logarítmica para exibir mais detalhes do espectro
-log_mag_spectrum = np.log(np.abs(shifted_fft_img))
+# Transformação logarítmica
+log_magnitude_spectrum = np.log(shifted_magnitude_spectrum)
 
-# Plotar as imagens resultantes
+# Plot dos resultados
 plt.subplot(221),plt.imshow(img, cmap = 'gray')
 plt.title('Imagem Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(222),plt.imshow(mag_spectrum, cmap = 'gray')
-plt.title('Magnitude do Espectro - sem padding'), plt.xticks([]), plt.yticks([])
-plt.subplot(223),plt.imshow(padded_mag_spectrum, cmap = 'gray')
-plt.title('Magnitude do Espectro - com padding'), plt.xticks([]), plt.yticks([])
-plt.subplot(224),plt.imshow(log_mag_spectrum, cmap = 'gray')
-plt.title('Magnitude do Espectro - shift e logarítmico'), plt.xticks([]), plt.yticks([])
+plt.subplot(222),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Espectro sem Padding'), plt.xticks([]), plt.yticks([])
+plt.subplot(223),plt.imshow(shifted_magnitude_spectrum, cmap = 'gray')
+plt.title('Espectro com Padding e Shift'), plt.xticks([]), plt.yticks([])
+plt.subplot(224),plt.imshow(log_magnitude_spectrum, cmap = 'gray')
+plt.title('Espectro com Padding, Shift e Transformação Logarítmica'), plt.xticks([]), plt.yticks([])
 plt.show()
